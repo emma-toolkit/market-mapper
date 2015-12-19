0fd6a48afa3a52999eb280571dc493e8c8ed79d7
jaschaephraim <jascha@jaschaephraim.com>
2015-12-18 23:13:28 -0800

    Quick and dirty display of sample data

--- app/app.js
+++ app/app.js
@@ -3,39 +3,43 @@
 import D3 from 'd3'
 import data from '../dev/data.json'
 
-let nodes = {
+let nodes, elements;
+nodes = elements = {
   environment: [],
   chain: [],
   infrastructure: []
 }
-for(let id in data) {
+for (let id in data) {
   let node = data[id];
   nodes[node.type].push(node);
 }
 
 document.body.style.height = `${window.innerHeight}px`;
-
 appendContainer('environment');
 appendContainer('chain');
 appendContainer('infrastructure');
 
-D3.select('#environment').selectAll('div')
+elements.environment = D3.select('#environment').selectAll('div')
   .data(nodes.environment)
   .enter().append('div')
   .attr('class', 'node')
   .text(d => d.name);
-
-D3.select('#chain').selectAll('div')
+elements.chain = D3.select('#chain').selectAll('div')
   .data(nodes.chain)
   .enter().append('div')
   .attr('class', 'node')
   .text(d => d.name);
-
-D3.select('#infrastructure').selectAll('div')
+elements.infrastructure = D3.select('#infrastructure').selectAll('div')
   .data(nodes.infrastructure)
   .enter().append('div')
   .attr('class', 'node')
   .text(d => d.name);
+
+elements.chain.each(function(d) {
+  d.x = this.offsetLeft;
+  d.y = this.offsetTop;
+  console.log(d);
+});
 
 function appendContainer(id) {
   let div = document.createElement('DIV');
