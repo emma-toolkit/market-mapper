53aa6e02492f244059f7288c690fe17069355697
jaschaephraim <jascha@jaschaephraim.com>
2016-01-07 14:20:12 -0800

    Resolve #3

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,34 +1,40 @@
 import { DefinePlugin, optimize } from 'webpack'
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 
+const entry = {
+  css: './styles/app.styl',
+  vendor: [
+    'bluebird',
+    'cytoscape',
+    'cytoscape-dagre',
+    'dagre',
+    'fast-csv',
+    'immutable',
+    'localforage',
+    'lodash.debounce',
+    'react',
+    'react-dom',
+    'react-redux',
+    'redux',
+    'redux-actions',
+    'redux-immutablejs',
+    'redux-promise'
+  ],
+  app: './app/app.js'
+};
+const commons = ['vendor'];
+if (process.env.NODE_ENV === 'development') {
+  entry.dev = [
+    'redux-devtools',
+    'redux-devtools-dock-monitor',
+    'redux-devtools-log-monitor'
+  ];
+  commons.push('dev');
+}
+
 export default {
   // context: `${process.cwd()}/app`,
-  entry: {
-    css: './styles/app.styl',
-    vendor: [
-      'bluebird',
-      'cytoscape',
-      'cytoscape-dagre',
-      'dagre',
-      'fast-csv',
-      'immutable',
-      'localforage',
-      'lodash.debounce',
-      'react',
-      'react-dom',
-      'react-redux',
-      'redux',
-      'redux-actions',
-      'redux-immutablejs',
-      'redux-promise'
-    ],
-    dev: [
-      'redux-devtools',
-      'redux-devtools-dock-monitor',
-      'redux-devtools-log-monitor'
-    ],
-    app: './app/app.js'
-  },
+  entry,
   output: {
     path: './build',
     filename: '[name].js'
@@ -46,7 +52,7 @@ export default {
     new DefinePlugin({
       'process.env.NODE_ENV': `'${process.env.NODE_ENV}'`
     }),
-    new optimize.CommonsChunkPlugin({names:['vendor', 'dev']}),
+    new optimize.CommonsChunkPlugin({names: commons}),
     new HTMLWebpackPlugin({title: 'EMMA Toolkit'})
   ],
   devServer: {
