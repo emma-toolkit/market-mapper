c72b9bd3757a951289b3a954b8bc0441a6e5ef26
jaschaephraim <jascha@jaschaephraim.com>
2016-02-13 23:37:08 -0800

    Update csv importer

--- src/csv.js
+++ src/csv.js
@@ -15,17 +15,10 @@ export default function(element_map) {
   });
   for (let [id, d] of element_map) {
     if (d.element === 'node') {
-      let position = null;
-      if (d.in === -1) {
-        position = 'initial';
-      } else if (d.out === -1) {
-        position = 'final';
-      }
       state = state.setIn(['nodes', d.domain, d.id], Node({
         domain: d.domain,
         id: d.id,
         label: d.label,
-        position: position,
         disruption: parseInt(d.disruption) || 0,
         x: parseFloat(d.x) || 0,
         y: parseFloat(d.y) || 0
